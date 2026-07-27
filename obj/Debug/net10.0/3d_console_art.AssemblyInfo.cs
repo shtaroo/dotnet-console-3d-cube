5a5f838a96e648cc5dd70dfe0e302ab1050470ad
Casper <Casper.meier.jenkins@gmail.com>
2026-07-27 18:39:25 +0200

    added gitignore

--- obj/Debug/net10.0/3d_console_art.AssemblyInfo.cs
+++ obj/Debug/net10.0/3d_console_art.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3d_console_art")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6ef0568a3a292bd8e99431d081b1e07142e2dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("3d_console_art")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3d_console_art")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
